3dd0cd68655e6d7c9f0ac55a67182eb45ba23812
todder02 <tmantell99@yahoo.com>
2017-01-11 09:20:32 -0600

    Expanding description of slice vs splice."

--- nonMutatingFunctions.js
+++ nonMutatingFunctions.js
@@ -2,7 +2,7 @@
 const myArray = [1,2,3,4,5,6];
 console.log(`original array values: ${myArray}`);
 
-//concat flattens two arrays into one without mutating the original arrays
+//.concat flattens two arrays into one without mutating the original arrays
 let flattened = myArray.concat([7,8,9,10]);
 console.log(`flattened values: ${flattened}`);
 
@@ -16,7 +16,7 @@ let filtered = myArray.filter((item) => {
   return item > 1 && item < 6;
 });
 
-//reduce takes 4 arguments, 1st is the accumulator (we are using ES6 default to 0)
+//.reduce takes 4 arguments, 1st is the accumulator (we are using ES6 default to 0)
 let reduced = myArray.reduce((accumulator = 0, currentValue) => {
   return accumulator + currentValue;
 });
@@ -31,10 +31,12 @@ console.log(`filtered values: ${filtered}`);
 console.log(`reduced values: ${reduced}`);
 console.log(`found value: ${found}`);
 
-//array.prototype.every checks the entire array and return true or false
+//array.prototype.every checks the entire array and return true or false if all items in the array match the criteria
 console.log(myArray.every((item) => {
   return item < 10;
 }));
 
+//.slice returns a new array with items starting at the index and taking the number of items specified by the second param
+//Unlike SPLICE, it does NOT mutate the original array.  For that reason, favor slice over sPlice.
 let sliced = myArray.slice(1,3);
 console.log(`sliced values: ${sliced}`);
